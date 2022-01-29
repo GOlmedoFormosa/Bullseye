b80b05d45fc3c8f6f797054137fc1add4dca0404
gustavo <gustavo.olmedo.formosa@gmail.com>
2022-01-29 20:49:28 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by gustavo on 29/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
